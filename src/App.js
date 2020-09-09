5c3dc04f5c10177f6bef1ef42d3e6fc02d45d8d6
Prabhjyot Singh <prabhjyotg@gmail.com>
2020-09-09 12:20:52 -0400

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    //BEM naming convention
+    <div className="app">
+      <h1>facebook clone!</h1>
+      {/* Header Component*/}
+      {/*App Body*/}
+      {/*Side Bar*/}
+      {/*Feed*/}
+      {/*Widgets*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBR9LPohY0yB7Jfk0SgJkXuAf5MS-bjbMI",
+    authDomain: "facebook-clone-app-bcb15.firebaseapp.com",
+    databaseURL: "https://facebook-clone-app-bcb15.firebaseio.com",
+    projectId: "facebook-clone-app-bcb15",
+    storageBucket: "facebook-clone-app-bcb15.appspot.com",
+    messagingSenderId: "711631022369",
+    appId: "1:711631022369:web:0d7ba949cf30f61a36b660",
+    measurementId: "G-VD5WQ4EMCW"
+  };
